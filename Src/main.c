a9a0e5726241a1937976c3d794ec776c21ad1029
David Gelkin <david.gelkin@amulethotkey.com>
2021-11-23 17:31:27 +0000

    initial commit

---
+++ Src/main.c
@@ -0,0 +1,242 @@
+/*
+ * This is a USART app that receives a cmd via the users keyboard to toggle on/off an LED.
+ * @author David Gelkin <david.gelkin@gmail.com>
+ */
+
+#include "stm32f4xx.h"
+#include "main.h"
+#include "string.h"
+#include "stdlib.h"
+
+typedef struct LEDlamp;
+void RCC_init(void);
+void USART_init(void);
+void statusLED(int onoff);
+void GPIO_init(void);
+void USART_write(int ch);
+char USART_read(void);
+char* printMenu(void);
+void USART_write_string(char* charString);
+
+typedef enum {
+    LEDOnFULL,
+    LEDOFFFULL,
+    NoOfIlluminationStates
+}illuminationState;
+
+typedef enum {
+    emptyLED,
+    LD1,
+    noOfLED
+} ledID;
+
+typedef struct{
+    char *location;
+    ledID GPIO;
+    illuminationState ledstate;
+} LEDlamp;
+
+
+#define ON  1
+#define OFF 0
+
+// Global
+LEDlamp led1;
+LEDlamp led2;
+LEDlamp led3;
+
+char* LEDONSTRING  = "LED 'should' be ON.\r\n";
+char* LEDOFFSTRING = "LED 'should' be OFF.\r\n";
+char* CLEARDISPLAY = "\n\n\n\n\n\n\n\n\n\n";
+char* APPHELP = "\n -APP HELP- \r\nThis is a demo app using USART to illuminate an LED.\r\n"
+                "The LED can also be toggled by pressing the onboard default Button.\r\n";
+
+/*
+ * Initialise the RCC clocks
+ */
+void RCC_init(void){
+    // clocks
+    RCC->AHB1ENR |= GPIOAEN;     // GPIOA clock enable PA2 = Tx
+    RCC->APB1ENR |= USART2EN;    // USART2 clock enable 0x20000
+}
+
+/*
+ * GPIO Init
+ */
+void GPIO_init(void){
+    // gpio AF PA2
+    GPIOA->AFR[0] &= ~0x0F00;    // clear
+    GPIOA->AFR[0] |= 0x7700;     // set AF7 function
+
+    GPIOA->MODER &= ~0x0CF0;     // clear
+    GPIOA->MODER |= 0x04A0;      // set to AF mode 10b PA2(Tx) PA3(Rx) & PA5(LED) as output 01b
+
+}
+
+/*
+ * Initialise the USART
+ */
+void USART_init(void){
+    // Configure USART2
+    USART2->BRR = 0x0683;        // set baud 9600
+    USART2->CR1 = TE | RE;       // set TE & RE
+    USART2->CR2 = 0x0000;
+    USART2->CR3 = 0x0000;
+    USART2->CR1 |= UE;       // Enable USART2
+}
+
+/*
+ * LED ON PA5
+ */
+void statusLED(int onoff){
+    if(onoff==ON) {
+        // Devboard example has ONLY 1 test LED but imagine more LEDs can be tied to an LED object...
+        GPIOA->BSRR |= 0x0020;       // LED on
+        led1.ledstate = ON;
+    }else{
+        GPIOA->BSRR |= 0x0020 << 16; // LED off
+        led1.ledstate = OFF;
+    }
+}
+
+/*
+ * write to USART a single character
+ */
+void USART_write(int ch){
+    while(!(USART2->SR & TXE)){}
+        USART2->DR = (ch & 0xFF);
+}
+
+/*
+ * write to USART a string of characters
+ */
+void USART_write_string(char* charString){
+    for(int a = 0; a<strlen(charString);a++ ) {
+        USART_write(charString[a]);
+    }
+}
+
+
+/*
+ * read USART
+ */
+char USART_read(void){
+    while(!(USART2->SR & RXNE)){}
+    return USART2->DR;
+}
+
+/*
+ * Hard coded menu of options to control app
+ */
+char* printMenu(void){
+    char *menu = "\r\n------------------------------\r\n"
+                 "*           MENU             *\r\n"
+                 "------------------------------\r\n"
+                 "    1    - \tLED ON in Kitchen\r\n"
+                 "    0    - \tLED OFF in Kitchen\r\n"
+                 "spacebar - \tDisplay this menu\r\n"
+                 " h or H  - \tDisplay app help\r\n"
+                 " s or S  - \tPrint status of LED\r\n"
+                 "   c     - \tClear display with 10* line feeds\r\n"
+                 "\r\n"
+                 " esc, q 0r Q  - \tQuit\r\n"
+                 "\r\n"
+                 "\r\n";
+    return menu;
+}
+
+/*
+ * Main function
+ */
+int main(void){
+    RCC_init();
+    GPIO_init();
+    USART_init();
+    char c;
+
+    // object to hold status of LED ~led1
+    led1.GPIO = 1;  // LD1
+    led1.ledstate = OFF;
+    led1.location = malloc(8*sizeof(char));
+    strcpy(led1.location, "Kitchen");
+
+    // object to hold status of LED ~led2
+    led2.GPIO = 2;  // LD1
+    led2.ledstate = OFF;
+    led2.location = malloc(8*sizeof(char));
+    strcpy(led2.location, "Bedroom");
+
+    // object to hold status of LED ~led3
+    led3.GPIO = 3;  // LD1
+    led3.ledstate = OFF;
+    led3.location = malloc(8*sizeof(char));
+    strcpy(led3.location, "Porch");
+
+
+    int APPisOn = ON;
+
+    //print initial menu as app starts
+    USART_write_string(printMenu());
+
+    // Super loop
+    while(APPisOn){
+        c = USART_read();
+        //USART_write(c); // loopback for debugging
+
+        // turn LED ON
+        if(c == '1') {
+            statusLED(ON);
+            //USART_write_string(LEDONSTRING); // Debug
+        }
+
+        // 0 turn LED OFF
+        if(c =='0') {
+            statusLED(OFF);
+            //USART_write_string(LEDOFFSTRING); // Debug
+        }
+
+        // Spacebar, q or h displays the menu!
+        if(c == 32) {
+            // turn LED off as default
+            statusLED(OFF);
+            USART_write_string(printMenu());
+        }
+
+        // h displays the app help
+        if((c == 'h')||(c=='H')) {
+            // turn LED off as default
+            statusLED(OFF);
+            USART_write_string(APPHELP);
+        }
+
+        // LED status
+        if((c == 's')||(c=='S')) {
+            // turn LED off as default
+            char *status = malloc(20*sizeof(char));
+            strcpy(status,"[STATUS CHECK] \r\n");
+            USART_write_string(status);
+            USART_write_string(led1.location);
+            USART_write_string(led1.ledstate==0?"\tOFF\r\n":"\tON\r\n");
+            USART_write_string(led2.location);
+            USART_write_string(led2.ledstate==0?"\tOFF\r\n":"\tON\r\n");
+            USART_write_string(led3.location);
+            USART_write_string(led3.ledstate==0?"\tOFF\r\n":"\tON\r\n");
+
+            USART_write_string("\r\n");
+            free(status);
+        }
+
+        //TODO clear display with 10x new lines!
+        if(c == 'c') {
+            // turn LED off as default
+            statusLED(OFF);
+            USART_write_string(CLEARDISPLAY);
+        }
+
+        // quit app, press esc button
+        if((c == 27) || ((c == 'q')||(c=='Q'))){
+            APPisOn = OFF;
+            USART_write_string("\nAPP SHUTDOWN\n\n");
+        }
+    }
+}
---
+++ cmake-build-debug/CMakeFiles/USART3.elf.dir/DependInfo.cmake
@@ -0,0 +1,82 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/labtest/CLionProjects/USART3/Startup/startup_stm32f446retx.s" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Startup/startup_stm32f446retx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F446xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/labtest/CLionProjects/USART3/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "/home/labtest/CLionProjects/USART3/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "/home/labtest/CLionProjects/USART3/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "/home/labtest/CLionProjects/USART3/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "/home/labtest/CLionProjects/USART3/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "/home/labtest/CLionProjects/USART3/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "/home/labtest/CLionProjects/USART3/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "/home/labtest/CLionProjects/USART3/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "/home/labtest/CLionProjects/USART3/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "/home/labtest/CLionProjects/USART3/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "/home/labtest/CLionProjects/USART3/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "/home/labtest/CLionProjects/USART3/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "/home/labtest/CLionProjects/USART3/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "/home/labtest/CLionProjects/USART3/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "/home/labtest/CLionProjects/USART3/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "/home/labtest/CLionProjects/USART3/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
+  "/home/labtest/CLionProjects/USART3/Src/main.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Src/main.c.obj"
+  "/home/labtest/CLionProjects/USART3/Src/stm32f4xx_hal_msp.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Src/stm32f4xx_hal_msp.c.obj"
+  "/home/labtest/CLionProjects/USART3/Src/stm32f4xx_it.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Src/stm32f4xx_it.c.obj"
+  "/home/labtest/CLionProjects/USART3/Src/syscalls.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Src/syscalls.c.obj"
+  "/home/labtest/CLionProjects/USART3/Src/sysmem.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Src/sysmem.c.obj"
+  "/home/labtest/CLionProjects/USART3/Src/system_stm32f4xx.c" "/home/labtest/CLionProjects/USART3/cmake-build-debug/CMakeFiles/USART3.elf.dir/Src/system_stm32f4xx.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32F446xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
